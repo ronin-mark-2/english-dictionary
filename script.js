7c74fa0833d47901528b52d75125a3272e5c3065
ronin-mark-2 <161806297+ronin-mark-2@users.noreply.github.com>
2024-09-01 11:10:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector('.wrapper');
+const searchInput = wrapper.querySelector('.input');
+const infoText = wrapper.querySelector('.info-text');
+const synonyms = wrapper.querySelector('.synonyns .list');
+const removeIcon = wrapper.querySelector('.search span');
+
